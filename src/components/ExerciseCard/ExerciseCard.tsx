113333cb4c6ac3d569e9a856a85ec54467e7536b
emrekrn <136122266+emrekrn@users.noreply.github.com>
2023-11-01 21:21:44 +0100

    Exercise card has been created

---
+++ src/components/ExerciseCard/ExerciseCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './exerciseCard.scss';
+
+const ExerciseCard = () => {
+	return (
+		<div className='exercise-card bg-primary p-3'>
+			<div className='exercise-name text-white'>Bench Press</div>
+		</div>
+	);
+};
+
+export default ExerciseCard;
--- src/components/WorkoutDetails/WorkoutDetails.tsx
+++ src/components/WorkoutDetails/WorkoutDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './workoutDetails.scss';
 import workoutPlaceholder from '../../images/workout-placeholder.jpg';
+import ExerciseCard from '../ExerciseCard/ExerciseCard';
 
 const WorkoutDetails = () => {
 	return (
@@ -10,6 +11,26 @@ const WorkoutDetails = () => {
 			</div>
 			<div className='workout-content align-self-start'>
 				<h2 className='text-white mt-4'>[Workout name]</h2>
+				<p className='text-grey'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+					assumenda magni voluptatum harum unde fuga neque excepturi
+					consequuntur praesentium quos rerum, a laborum nobis. Laboriosam
+					recusandae, ipsam rem numquam neque accusantium harum nisi at illo,
+					ipsum, perferendis magni praesentium saepe labore corporis suscipit
+					sint. Dolores recusandae totam,
+				</p>
+
+				<div className='exercises-field mt-4'>
+					<h3 className='text-white'>Exercises:</h3>
+					<hr className='text-white' />
+					<div className='exercises-list-field d-flex gap-3 flex-column'>
+						<div className='sort-btn align-self-end bg-primary d-flex align-items-center justify-content-center'>
+							<span className='text-white'>By name</span>
+						</div>
+						<ExerciseCard />
+						<ExerciseCard />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
